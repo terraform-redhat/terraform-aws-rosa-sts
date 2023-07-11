2975ae1998daae247594fee7959c039e7f442ecd
Danielle Barda <dbarda@redhat.com>
2023-07-11 14:37:58 +0300

    OCM-2361 Support generated account_role_prefix

--- account_roles_creation/main.tf
+++ account_roles_creation/main.tf
@@ -7,11 +7,21 @@ terraform {
   }
 }
 
+resource "random_string" "default_random" {
+  length  = 4
+  special = false
+  upper   = false
+}
+
+locals {
+  account_role_generated_if_needed = var.account_role_prefix != ""? var.account_role_prefix : "account-role-${random_string.default_random.result}"
+}
+
 module "rosa_account_roles" {
   source = "./account_role"
   count  = 2
 
-  account_role_prefix              = var.account_role_prefix
+  account_role_prefix              = local.account_role_generated_if_needed
   rosa_openshift_version           = var.rosa_openshift_version
   account_role_properties          = local.account_roles_properties[count.index]
   instance_account_role_properties = local.instance_account_roles_properties[count.index]
@@ -25,7 +35,7 @@ module "rosa_operator_role_policies" {
   source = "./operator_role_policy"
   count  = 6
 
-  account_role_prefix             = var.account_role_prefix
+  account_role_prefix             = local.account_role_generated_if_needed
   rosa_openshift_version          = var.rosa_openshift_version
   operator_role_policy_properties = local.operator_roles_policy_properties[count.index]
   tags                            = var.tags
@@ -63,42 +73,42 @@ locals {
   # TODO: if there is a new policy for a new OCP versions, need to add it here also
   operator_roles_policy_properties = [{
     # openshift-machine-api
-    policy_name    = substr("${var.account_role_prefix}-openshift-cloud-network-config-controller-cloud-credentials", 0, 64)
+    policy_name    = substr("${local.account_role_generated_if_needed}-openshift-cloud-network-config-controller-cloud-credentials", 0, 64)
     policy_details = var.operator_role_policies["openshift_cloud_network_config_controller_cloud_credentials_policy"]
     namespace      = "openshift-cloud-network-config-controller"
     operator_name  = "cloud-credentials"
     },
     {
       # openshift-cloud-credential-operator
-      policy_name    = substr("${var.account_role_prefix}-openshift-machine-api-aws-cloud-credentials", 0, 64)
+      policy_name    = substr("${local.account_role_generated_if_needed}-openshift-machine-api-aws-cloud-credentials", 0, 64)
       policy_details = var.operator_role_policies["openshift_machine_api_aws_cloud_credentials_policy"]
       namespace      = "openshift-machine-api"
       operator_name  = "aws-cloud-credentials"
     },
     {
       # openshift-cloud-network-config-controller
-      policy_name    = substr("${var.account_role_prefix}-openshift-cloud-credential-operator-cloud-credential-operator-iam-ro-creds", 0, 64)
+      policy_name    = substr("${local.account_role_generated_if_needed}-openshift-cloud-credential-operator-cloud-credential-operator-iam-ro-creds", 0, 64)
       policy_details = var.operator_role_policies["openshift_cloud_credential_operator_cloud_credential_operator_iam_ro_creds_policy"]
       namespace      = "openshift-cloud-credential-operator"
       operator_name  = "cloud-credential-operator-iam-ro-creds"
     },
     {
       # openshift-image-registry
-      policy_name    = substr("${var.account_role_prefix}-openshift-image-registry-installer-cloud-credentials", 0, 64)
+      policy_name    = substr("${local.account_role_generated_if_needed}-openshift-image-registry-installer-cloud-credentials", 0, 64)
       policy_details = var.operator_role_policies["openshift_image_registry_installer_cloud_credentials_policy"]
       namespace      = "openshift-image-registry"
       operator_name  = "installer-cloud-credentials"
     },
     {
       # openshift-ingress-operator
-      policy_name    = substr("${var.account_role_prefix}-openshift-ingress-operator-cloud-credentials", 0, 64)
+      policy_name    = substr("${local.account_role_generated_if_needed}-openshift-ingress-operator-cloud-credentials", 0, 64)
       policy_details = var.operator_role_policies["openshift_ingress_operator_cloud_credentials_policy"]
       namespace      = "openshift-ingress-operator"
       operator_name  = "cloud-credentials"
     },
     {
       # openshift-cluster-csi-drivers
-      policy_name    = substr("${var.account_role_prefix}-openshift-cluster-csi-drivers-ebs-cloud-credentials", 0, 64)
+      policy_name    = substr("${local.account_role_generated_if_needed}-openshift-cluster-csi-drivers-ebs-cloud-credentials", 0, 64)
       policy_details = var.operator_role_policies["openshift_cluster_csi_drivers_ebs_cloud_credentials_policy"]
       namespace      = "openshift-cluster-csi-drivers"
       operator_name  = "ebs-cloud-credentials"
